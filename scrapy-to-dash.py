044e4ec20443bdcd35af25aeb86caf0222ebeef5
Aziz Alto <iamaziz.alto@gmail.com>
2015-09-08 11:54:27 -0400

    Update version 0.22 to 1.00

--- scrapy-to-dash.py
+++ scrapy-to-dash.py
@@ -1,4 +1,7 @@
-import requests, sqlite3, os, urllib
+import requests
+import sqlite3
+import os
+import urllib
 from bs4 import BeautifulSoup as bs
 
 
@@ -8,13 +11,13 @@ cmdcommand = """ cd . && rm -rf Scrapy.docset && mkdir -p Scrapy.docset/Contents
 os.system(cmdcommand)
 
 
-
 # CONFIGURATION
 docset_name = 'Scrapy.docset'
 output = docset_name + '/Contents/Resources/Documents/'
 
 # create docset directory
-if not os.path.exists(output): os.makedirs(output)
+if not os.path.exists(output):
+    os.makedirs(output)
 
 # add icon
 icon = 'http://upload.wikimedia.org/wikipedia/commons/7/7f/Smile_icon.png'
@@ -23,77 +26,88 @@ urllib.urlretrieve(icon, docset_name + "/icon.png")
 
 def update_db(name, typ, path):
 
-	try:
-	  cur.execute("SELECT rowid FROM searchIndex WHERE path = ?", (path,))
-	  dbpath = cur.fetchone()
-	  cur.execute("SELECT rowid FROM searchIndex WHERE name = ?", (name,))
-	  dbname = cur.fetchone()
+    try:
+        cur.execute("SELECT rowid FROM searchIndex WHERE path = ?", (path,))
+        dbpath = cur.fetchone()
+        cur.execute("SELECT rowid FROM searchIndex WHERE name = ?", (name,))
+        dbname = cur.fetchone()
 
-	  if dbpath is None and dbname is None:
-	      cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, typ, path))
-	      print('DB add >> name: {0} | type: {1} | path: {3}'.format(name, typ, path))
-	  else:
-	      print("record exists")
-	except:
-		pass
+        if dbpath is None and dbname is None:
+            cur.execute(
+                'INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, typ, path))
+            print(
+                'DB add >> name: {0} | type: {1} | path: {3}'.format(name, typ, path))
+        else:
+            print("record exists")
+    except:
+        pass
 
 
 def add_urls():
 
-  # index pages
-  pages = {
-    'Section'       : 'http://doc.scrapy.org/en/latest/',
-    'func'          : 'http://doc.scrapy.org/en/latest/genindex.html',
-    'Command'       : 'http://doc.scrapy.org/en/latest/topics/commands.html',
-    'Module'        : 'http://doc.scrapy.org/en/latest/py-modindex.html',
-      }
+    # index pages
+    pages = {
+        'func': 'http://doc.scrapy.org/en/latest/genindex.html',
+        'Command': 'http://doc.scrapy.org/en/latest/topics/commands.html',
+        'Module': 'http://doc.scrapy.org/en/latest/py-modindex.html',
+        'Guide': 'http://doc.scrapy.org/en/latest/intro/overview.html'
+    }
 
-  # loop through index pages:
-  for p in pages:
-    base_path = 'doc.scrapy.org/en/latest/'
-    typ = p
-    # soup each index page
-    html = requests.get(pages[p]).text
-    soup = bs(html)
-    
-    for a in soup.findAll('a'):
-      name = a.text.strip()
-      name = name.replace('\n', '')
+    # loop through index pages:
+    for p in pages:
+        base_path = 'doc.scrapy.org/en/latest/'
+        typ = p
+        # soup each index page
+        html = requests.get(pages[p]).text
+        soup = bs(html)
 
-      if len(name) > 2:
-        path = a.get('href')
-        
-        filtered = ('index.html', 'http', '//', 'irc:', 'spiders.html', 'benchmarking.html', 'shell.html')
-        dirpath = ['Command']
+        for a in soup.findAll('a'):  # , class_='reference internal'):
+            name = a.text.strip()
+            name = name.replace('\n', '')
 
-        if '#' in path and len(path) > 2 and not path.startswith(filtered):
-          if typ in dirpath:
-            base_path = pages[p].replace('http://','')
-          path = base_path + path
-          update_db(name, typ, path)
+            if len(name) > 2:
+                path = a.get('href')
+
+                filtered = ('index.html', 'http', '//', 'irc:',
+                            'spiders.html', 'benchmarking.html', 'shell.html')
+                dirpath = ['Command', 'Guide']
+
+                if '#' in path and len(path) > 2 and not path.startswith(filtered):
+                    if typ in dirpath:
+                        base_path = pages[p].replace('http://', '')
+                    if '.html' in path:
+                        base_path = base_path.replace(
+                            base_path.split('/')[-1], '')
+                    path = base_path + path
+
+                    if 'intro/overview.html../' in path:
+                        path = path.replace('intro/overview.html../', '')
+
+                    update_db(name, typ, path)
 
 
 def add_infoplist():
-  name = docset_name.split('.')[0]
-  info = " <?xml version=\"1.0\" encoding=\"UTF-8\"?>" \
-         "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"> " \
-         "<plist version=\"1.0\"> " \
-         "<dict> " \
-         "    <key>CFBundleIdentifier</key> " \
-         "    <string>{0}</string> " \
-         "    <key>CFBundleName</key> " \
-         "    <string>{1}</string>" \
-         "    <key>DocSetPlatformFamily</key>" \
-         "    <string>{2}</string>" \
-         "    <key>isDashDocset</key>" \
-         "    <true/>" \
-         "    <key>isJavaScriptEnabled</key>" \
-         "    <true/>" \
-         "    <key>dashIndexFilePath</key>" \
-         "    <string>{3}</string>" \
-         "</dict>" \
-         "</plist>".format(name, name, name, 'doc.scrapy.org/en/latest/' + 'index.html')
-  open(docset_name + '/Contents/info.plist', 'wb').write(info)
+    name = docset_name.split('.')[0]
+    info = " <?xml version=\"1.0\" encoding=\"UTF-8\"?>" \
+           "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"> " \
+           "<plist version=\"1.0\"> " \
+           "<dict> " \
+           "    <key>CFBundleIdentifier</key> " \
+           "    <string>{0}</string> " \
+           "    <key>CFBundleName</key> " \
+           "    <string>{1}</string>" \
+           "    <key>DocSetPlatformFamily</key>" \
+           "    <string>{2}</string>" \
+           "    <key>isDashDocset</key>" \
+           "    <true/>" \
+           "    <key>isJavaScriptEnabled</key>" \
+           "    <true/>" \
+           "    <key>dashIndexFilePath</key>" \
+           "    <string>{3}</string>" \
+           "</dict>" \
+           "</plist>".format(name, name, name,
+                             'doc.scrapy.org/en/latest/' + 'index.html')
+    open(docset_name + '/Contents/Info.plist', 'wb').write(info)
 
 # create and connect to SQLite
 db = sqlite3.connect(docset_name + '/Contents/Resources/docSet.dsidx')
@@ -102,8 +116,10 @@ try:
     cur.execute('DROP TABLE searchIndex;')
 except:
     pass
-    cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
-    cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+    cur.execute(
+        'CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+    cur.execute(
+        'CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
 
 # start
 add_urls()
